e00b0fdc5384ec827c3f3a9ed1f9aa4cd3c79f22
gowtham-0116 <gowthamkomagan0116@gmail.com>
2024-11-13 10:05:12 +0530

    Create 15.EXAMPLE FOR INT DATATYPE.sv

---
+++ ASSIGNMENT/ASSIGNMENT 2/15.EXAMPLE FOR INT DATATYPE.sv
@@ -0,0 +1,18 @@
+//15. Predict the output for the below code
+
+module top;
+int num1;
+int num2;
+initial
+
+begin
+num1 = int'(10.0 - 1.8); // Case 1
+  $display("The integer value is : %d",num1);
+num2 = int'(5/3); // Case 2
+  $display("The integer value is : %d",num2);
+end
+endmodule
+
+/*Output
+The integer value is :           8
+The integer value is :           1 */
