215d5bd2b9d4eebce4a2c8150f7233def76f4b1b
gowtham-0116 <gowthamkomagan0116@gmail.com>
2024-11-20 14:06:34 +0530

    Create 20.INHERITANCE EXAMPLE USING 'this' KEYWORD.sv

---
+++ ASSIGNMENT/ASSIGNMENT 3/20.INHERITANCE EXAMPLE USING 'this' KEYWORD.sv
@@ -0,0 +1,27 @@
+//20. Predict the output for the following exercises
+
+class trans;
+	int i;
+	function new(int i = 10);
+		this.i = i;
+	endfunction
+endclass
+
+class ex_trans1 extends trans;
+	int i;
+	function new();
+		i = 20;
+	endfunction
+endclass
+
+ex_trans1 e_h;
+
+module abc();
+	initial begin
+		e_h = new();
+      $display("%p",e_h);
+	end
+endmodule
+
+/*Output
+'{super:'{i:10}, i:20}*/
