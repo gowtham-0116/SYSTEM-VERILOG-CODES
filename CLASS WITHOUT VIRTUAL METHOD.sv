837e06c7e6a8fc4286ebcb8371c0603711a494e4
Gowtham <gowthamkomagan0116@gmail.com>
2024-12-19 10:49:06 +0530

    Create CLASS WITHOUT VIRTUAL METHOD.sv

---
+++ CLASS WITHOUT VIRTUAL METHOD.sv
@@ -0,0 +1,32 @@
+// Code your testbench here
+// or browse Examples
+class packet;
+  integer i=1;
+   function integer init();
+    init =i;
+  endfunction
+endclass
+
+class child_packet extends packet;
+  integer i=2;
+  function integer init();
+    init =-i;
+  endfunction
+endclass
+
+module class_example;
+  int j;
+  initial begin
+    child_packet c1=new();
+    //c1=;
+    packet p1 = c1;
+    $display("value of i = %0d",p1.i);
+    j=p1.init();
+    $display("value of j = %0d",j);
+  end
+endmodule
+
+/*OUTPUT
+value of i = 1
+value of j = 1*/
+  
